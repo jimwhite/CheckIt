c29e6fbdd2738e0e5bb11c97a4eea464ccd2dcdb
Jim White <james.paul.white@gmail.com>
2013-07-26 19:23:26 -0700

    Copy script for spreading authorized keys around.

--- bin/set_up_keys.sh
+++ bin/set_up_keys.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for a in ling572_01 ling572_02 ling572_03 ling572_04 ling572_05 ling572_06 ling572_07 ling572_08 ling572_09 ; do
-   cp -R ~ling572_00/.ssh ~$a ;
-done
+# scp checkit ling572_00@dryas:.ssh
 
+# ssh-copy-id -i checkit.pub ling572_00@dryas
+
+for a in ling572_01 ling572_02 ling572_03 ling572_04 ling572_05 ling572_06 ling572_07 ling572_08 ling572_09 ; do
+   sudo -u $a mkdir /home/$a/.ssh ;
+   sudo -u $a cp -R /home/ling572_00/authorized_keys /home/$a/.ssh ;
+done
