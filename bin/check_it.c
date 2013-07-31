3685989cd8be3ced80fd2cd323c576383e77a70f
Jim White <james.paul.white@gmail.com>
2013-07-31 14:28:43 -0700

    Leave the arguments alone and check_it.groovy directly.

--- bin/check_it.c
+++ bin/check_it.c
@@ -17,22 +17,8 @@ int main(int argc, char** argv) {
        // Use a nice clean PATH.
        char * envp[] = { "PATH=/bin:/usr/bin", user_evar, (char *) 0 };
 
-       char ** gargv = (char **) calloc((argc + 3), sizeof(char *));
-
-       // We'll run ling572_00's check_it.groovy as ling572_00.
-       gargv[0] = "/home2/jimwhite/Projects/Groovy/groovy-2.1.6/bin/groovy";
-       gargv[1] = "/home2/ling572_00/Projects/CheckIt/bin/check_it.groovy";
-       // gargv[2] = passwd->pw_name;
-
-       // Copy the args given by our caller.
-       int i;
-       for (i = 1; i < argc; ++i) gargv[i + 1] = argv[i];
-       
-       // for (i = 0; envp[i] ; ++i) printf("env[%d] : %s\n", i, envp[i]);
-       // for (i = 0; gargv[i]; ++i) printf("argv[%d] : %s\n", i, gargv[i]);
-
        // Do it!
-       errno = execve(gargv[0], gargv, envp);  
+       errno = execve("/home2/ling572_00/Projects/CheckIt/bin/check_it.groovy", argv, envp);
     }
 
     printf("An error occured %d\n", errno);  
