e0e50d3e259bf68135e758a2b4ff4884d0634dc5
Jim White <james.paul.white@gmail.com>
2013-11-19 12:23:55 -0800

    Check every model after it's been tidied.

--- bin/check_570_hw6.sh
+++ bin/check_570_hw6.sh
@@ -13,9 +13,11 @@ export PATH=$CHECKIT_HOME/bin:$GROOVY_HOME/bin:$PATH
 ling570_hw6.groovy $1
 
 tidy_hmm.groovy <checker/2g_hmm >checker/tidy_2g_hmm
+check_hmm.groovy checker/tidy_2g_hmm checker/checked_2g_hmm
 diff_hmm.groovy ~/l570/hw6/tidy_wsj.2g_hmm checker/tidy_2g_hmm >checker/diff_wsj_2g_hmm.txt
 
 tidy_hmm.groovy <checker/3g_hmm_118 >checker/tidy_3g_hmm_118
+check_hmm.groovy checker/tidy_3g_hmm_118 checker/checked_3g_hmm_118
 diff_hmm.groovy ~/l570/hw6/tidy_wsj.3g_hmm_0.1_0.1_0.8 checker/tidy_3g_hmm_118 >checker/diff_wsj_3g_hmm_118.txt
 diff_hmm.groovy ~/l570/hw6/tidy_jim.3g_hmm_0.1_0.1_0.8 checker/tidy_3g_hmm_118 >checker/diff_jim_3g_hmm_118.txt
 
@@ -25,7 +27,9 @@ cd $1
 cd .. 
 
 tidy_hmm.groovy <checker/1b.2g_hmm >checker/tidy_1b.2g_hmm
+check_hmm.groovy checker/tidy_1b.2g_hmm checker/checked_1b.2g_hmm
 diff_hmm.groovy ~/l570/hw6/tidy_jim.2g_1b_hmm checker/tidy_1b.2g_hmm >checker/diff_1b_2g_hmm.txt
 
 tidy_hmm.groovy <checker/3g_1b_hmm >checker/tidy_3g_1b_hmm
+check_hmm.groovy checker/tidy_3g_1b_hmm checker/checked_3g_1b_hmm
 diff_hmm.groovy ~/l570/hw6/tidy_jim.3g_1b_hmm checker/tidy_3g_1b_hmm >checker/diff_3g_1b_hmm.txt
