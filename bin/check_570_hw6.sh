b557030d8388b7fe75fa0e0a9b8b57e34b23c182
Jim White <james.paul.white@gmail.com>
2013-11-19 07:12:59 -0800

    Check 2g and 3g models for simple (1b) test data.

--- bin/check_570_hw6.sh
+++ bin/check_570_hw6.sh
@@ -10,7 +10,7 @@ export PATH=$CHECKIT_HOME/bin:$GROOVY_HOME/bin:$PATH
 # An exception from ling570_hw6.groovy on stderr complaining about about a "Broken pipe" is OK.
 # Exception in thread "Thread-2" org.codehaus.groovy.runtime.InvokerInvocationException: java.io.IOException: Broken pipe
 
-ling570_hw6.groovy $*
+ling570_hw6.groovy $1
 
 tidy_hmm.groovy <checker/2g_hmm >checker/tidy_2g_hmm
 diff_hmm.groovy ~/l570/hw6/tidy_wsj.2g_hmm checker/tidy_2g_hmm >checker/diff_wsj_2g_hmm.txt
@@ -19,3 +19,13 @@ tidy_hmm.groovy <checker/3g_hmm_118 >checker/tidy_3g_hmm_118
 diff_hmm.groovy ~/l570/hw6/tidy_wsj.3g_hmm_0.1_0.1_0.8 checker/tidy_3g_hmm_118 >checker/diff_wsj_3g_hmm_118.txt
 diff_hmm.groovy ~/l570/hw6/tidy_jim.3g_hmm_0.1_0.1_0.8 checker/tidy_3g_hmm_118 >checker/diff_jim_3g_hmm_118.txt
 
+cd $1
+./create_2gram_hmm.sh ../checker/1b.2g_hmm <~/l570/hw6/1b.word_pos
+./create_3gram_hmm.sh <~/l570/hw6/1b.word_pos ../checker/3g_1b_hmm 0.1 0.1 0.8 /dropbox/13-14/570/hw6/examples/unk_prob_sec22
+cd .. 
+
+tidy_hmm.groovy <checker/1b.2g_hmm >checker/tidy_1b.2g_hmm
+diff_hmm.groovy checker/tidy_1b.2g_hmm ~/l570/hw6/tidy_jim.2g_1b_hmm >checker/diff_1b_2g_hmm.txt
+
+tidy_hmm.groovy <checker/3g_1b_hmm >checker/tidy_3g_1b_hmm
+diff_hmm.groovy ~/l570/hw6/tidy_jim.3g_1b_hmm checker/tidy_3g_1b_hmm >checker/diff_3g_1b_hmm.txt
