46be8b1d02f282505ba3f9060c88dc7358d8265d
Jim White <james.paul.white@gmail.com>
2013-11-25 11:25:38 -0800

    Bump up the memory request.

--- bin/run_it.cmd
+++ bin/run_it.cmd
@@ -7,7 +7,7 @@
 
 Universe   = vanilla
 
-Environment = PATH=/home2/jimwhite/Projects/Groovy/groovy-2.1.6/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/opt/git/bin:/opt/scripts:/condor/bin;LD_LIBRARY_PATH=/opt/xerces/lib;LC_ALL=en_US.UTF-8
+Environment = JAVA_OPTS=-Xmx512m;PATH=/home2/jimwhite/Projects/Groovy/groovy-2.1.6/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/opt/git/bin:/opt/scripts:/condor/bin;LD_LIBRARY_PATH=/opt/xerces/lib;LC_ALL=en_US.UTF-8
 
 Executable  = $(CHECKIT_HOME)/bin/$(_PROJECT_ID).groovy
 Arguments   = run_it $(_PROJECT_ID)
@@ -15,5 +15,6 @@ Log         = run_it_log.txt
 Input       = $(_PROJECT_TAR_FILE)
 Output      = run_it_out.html
 Error	    = run_it_err.txt
+Request_Memory	    = 2000
 Notification=Error
 Queue
